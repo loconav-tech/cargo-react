d09516036d2146748734f15044c78c56f2d73d99
Abid Rahim <abid.rahim@loconav.com>
2019-07-02 18:25:43 +0530

    Implement storybook JSX addon

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,2 +1,4 @@
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
+import 'storybook-addon-jsx/register';
+// import '@types/storybook-addon-jsx/register';
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,7 @@
-import { configure } from '@storybook/react';
+import { configure, setAddon } from '@storybook/react';
+import JSXAddon from 'storybook-addon-jsx';
+
+setAddon(JSXAddon)
 const req = require.context('../src/components', true, /.stories.tsx$/);
 function loadStories() {
   req.keys().forEach(req);
